701ee8fece563e419cde1686019e32347983f8f6
gowtham555 <96039652+gowtham555@users.noreply.github.com>
2022-07-27 20:06:18 +0530

    Add files via upload
    python statements

---
+++ if and elif.py
@@ -0,0 +1,9 @@
+marks=int(input("enter the marks":))
+if marks>85 and marks<=100:
+    print("grade a")
+elif marks>70 and marks<=100:
+    print("grade b")
+elif marks>60 and marks<=100:
+    print("grade c")
+else :
+    print("fail")
